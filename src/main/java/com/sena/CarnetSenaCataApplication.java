5f09dc9604f93bcf66c6ff7e7d0065c0585ef64a
Fabril <adrianfabril@gmail.com>
2023-09-08 18:35:54 -0500

    codigo BASE

---
+++ src/main/java/com/sena/CarnetSenaCataApplication.java
@@ -0,0 +1,13 @@
+package com.sena;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarnetSenaCataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarnetSenaCataApplication.class, args);
+	}
+
+}
